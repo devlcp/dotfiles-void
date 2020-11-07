d58a0b51b1ad16dc6b7df8a0c96eed18a93ce352
devlcp <devlcp@tuta.io>
2020-11-07 17:01:51 -0600

    Terminal config

---
+++ config/tym/config.lua
@@ -0,0 +1,81 @@
+local tym = require('tym')
+
+-- set config individually
+tym.set('width', 100)
+
+tym.set('font', 'Iosevka Mayukai Medium 12')
+
+-- set by table
+tym.set_config({
+  title = 'tym',
+  shell = '/usr/bin/fish',
+  cursor_shape = 'underline',
+  cursor_blink_mode = 'on',
+  scrollback_length = 1000,
+  -- background_image = '/xd/Temporales/CONFIG/wallper/iron-man-fly-view.png',
+  autohide = true,
+  color_foreground = '#ebdbb2',
+})
+
+-- ATAJOS DE TECLADO
+
+tym.set_keymap('<Ctrl><Shift>o', function()
+  local h = tym.get('height')
+  tym.set('height', h + 1)
+  tym.notify('Set window height :' .. h)
+end)
+
+function update_alpha(delta)
+  r, g, b, a = tym.color_to_rgba(tym.get('color_background'))
+  a = math.max(math.min(1.0, a + delta), 0.0)
+  bg = tym.rgba_to_color(r, g, b, a)
+  tym.set('color_background', bg)
+  tym.notify(string.format('%s alpha to %f', (delta > 0 and 'Inc' or 'Dec'), a))
+end
+
+tym.set_keymaps({
+  ['<Ctrl><Shift>Up'] = function()
+    update_alpha(0.05)
+  end,
+  ['<Ctrl><Shift>Down'] = function()
+    update_alpha(-0.05)
+  end,
+})
+
+tym.set_hook('scroll', function(dx, dy, x, y)
+  if tym.check_mod_state('<Ctrl>') then
+    if dy > 0 then
+      s = tym.get('scale') - 10
+    else
+      s = tym.get('scale') + 10
+    end
+    tym.set('scale', s)
+    tym.notify('set scale: ' .. s .. '%')
+    return true
+  end
+  if tym.check_mod_state('<Shift>') then
+    update_alpha(dy < 0 and 0.05 or -0.05)
+    return true
+  end
+end)
+
+-- set by table
+-- tym.set_keymaps({
+--   ['<Ctrl><Shift>t'] = function()
+--     tym.reload()
+--     tym.notify('reload config')
+--   end,
+--   ['<Ctrl><Shift>v'] = function()
+--     -- reload and notify
+--     tym.send_key('<Ctrl><Shift>t')
+--   end,
+
+--   ['<Shift>y'] = function()
+--     tym.notify('Y has been pressed')
+--     return true -- notification is shown and `Y` will be inserted
+--   end,
+--   ['<Shift>w'] = function()
+--     tym.notify('W has been pressed')
+--     -- notification is shown but `W` is not inserted
+--   end,
+-- })
---
+++ config/tym/theme.lua
@@ -0,0 +1,28 @@
+local bg = 'rgba(1, 22, 39, 0.90)'
+local fg = 'rgba(172, 180, 194, 0.90)'
+
+return {
+  color_background = bg,
+  color_foreground = fg,
+  color_bold = fg,
+  color_cursor = fg,
+  color_cursor_foreground = bg,
+  color_highlight = fg,
+  color_highlight_foreground = bg,
+  color_0  = '#1D3B53',
+  color_1  = '#fc514e', 
+  color_2  = '#a1cd5e',
+  color_3  = '#e3d18a',--brown
+  color_4  = '#82aaff',
+  color_5  = '#c792ea',
+  color_6  = '#7fdbca',
+  color_7  = '#a1aab8',
+  color_8  = '#7c8f8f',
+  color_9  = '#ff5874',
+  color_10 = '#21c7a8',
+  color_11 = '#ECC48D',
+  color_12 = '#82aaff',
+  color_13 = '#ae81ff',
+  color_14 = '#7fdbca',
+  color_15 = '#d6deeb',
+}
