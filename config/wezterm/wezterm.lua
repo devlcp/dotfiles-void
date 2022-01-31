07ad09108cbad86cb9b99a27da30a68f519981ed
devlcp <devlcp@tuta.io>
2022-01-31 12:37:33 -0600

    New terminal

---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,13 @@
+local wez = require('wezterm')
+
+return {
+    color_scheme = "Ayu Mirage",
+    hide_tab_bar_if_only_one_tab = true,
+    window_decorations = "NONE", -- quita la barra de titulo
+    window_padding = {
+        left = 2,
+        right = 2,
+        top = 0,
+        bottom = 0,
+    }
+}
